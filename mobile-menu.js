482d7ca7ddcfb90b35ec719509259b83c89de5aa
Eric <esr358@gmail.com>
2019-01-07 10:33:28 -0800

    mobile menu expanded

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,3 +1,15 @@
-function myFunction(x) {
-  x.classList.toggle("change");
-}
+          function myFunction(x) {x.classList.toggle("change");}
+          
+          function openNav() {
+              var menu = document.getElementById("Menu");
+              var shift = document.getElementsByClassName("columnMiddle")
+          if (menu.style.width < "1") {
+              menu.style.display = "block";
+              menu.style.width = "50%";
+//              shift.style.width = "0%";
+          } else {
+              menu.style.display = "none";
+              menu.style.width = "0%";
+//              shift.style.width = "100%";
+              }
+          }  
