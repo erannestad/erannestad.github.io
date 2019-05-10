76be6e310577a1b42bd17e54556a165cfd0a274b
Eric <esr358@gmail.com>
2019-05-10 04:21:42 -0400

    lazy load

--- dropdown.js
+++ dropdown.js
@@ -7,9 +7,30 @@ $(document).ready(function(){
    });
 });
         
-$(function() { // called when page is done loading; you can have lots of these
-  $.ajax({
-    url: 'window.location.href',
-    success: function(data) { $('#load-me-later').html(data); }
-  });
+$(window).load(function () {
+    
+    $('.load-me-later').load('/script');
 });
+
+
+
+
+
+
+
+
+
+
+const lazyImages = document.querySelectorAll('.load-me-later');
+
+function lazyLoadImages() {
+   lazyImages.forEach((val,i) => {
+
+     let src = val.querySelector('source').getAttribute('srcset');
+     let image = new Image();
+     image.src = src;
+     val.append(image);
+
+
+   })
+ }
