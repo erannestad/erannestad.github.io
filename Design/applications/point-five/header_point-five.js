0e4707a296407e412247ab6f0711bbbf32c05dad
Eric <esr358@gmail.com>
2019-08-29 17:55:41 -0700

    point five

--- Design/applications/point-five/header_point-five.js
+++ Design/applications/point-five/header_point-five.js
@@ -1,6 +1,6 @@
 var Application = {
-        heading1:'Seattle Art Museum',
-        heading2:'Application: Junior Designer',
+        heading1:'Point Five',
+        heading2:'Application: Designer',
         body:'Attached is the portfolio section of my application. The reviewer(s) can navigate to the main site <a href="../../projects.html">here</a> should they like to explore further.<br><p class="subtext">ADDITIONAL LINKS:<br>github: <a href="https://github.com/erannestad">github.com/erannestad</a>  |  artist website: <a href="www.ericrannestad.com">ericrannestad.com</a></p><p>Thank you for your consideration.'
     };
 
