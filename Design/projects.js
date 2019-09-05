cb6a60197ee74bf2a97b34b38137bbd37cb388b5
Eric <esr358@gmail.com>
2019-09-05 16:30:02 -0700

    Update projects.js

--- Design/projects.js
+++ Design/projects.js
@@ -6,7 +6,7 @@ var ProjectList = [
         categories: ['art'],
         related: ['WTRST', 'everythingIsGoingToHaveToBePutBack', 'exhibitionCatalog'],
         date: 'April, 2019',
-        thumbnail:'design-portfolio/VSC/Full_thumbnail.jpg',
+        thumbnail:'design-portfolio/VSC/Full1.2_thumbnail.jpg',
         //////////////////
         description: 'Firmament Arch is part of a growing series of work about the constructed qualities of climate change and our relationship to the natural world in the face of ecological collapse. <p class="subtext">The series reimagines cosmological structures of the past: the pillars of heaven, the firmament, the great deep, the ocean of heaven, etc. as architectural backbones of our future infrastructure and defining forces in our relationship to nature. <br><br> In religious cosmology, the sky was conceptualized as a physical structure – a firmament – that separated earth from the oceans of heavens. Firmament Arch references the architecture of our increasingly necessary biodomes and greenhouses and reimagines them as the firmament structures of tomorrow. The curved backbone of the work houses a network of tanks, tubes, pumps, and nozzles that form an aeroponic growing environment, allowing plants to grow in irregular and confined spaces with minimal water waste or reliance on natural light.</p>',
         //////////////////
