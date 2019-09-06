d7521167f9db6ed6ab90c82af51cea57a16af321
Eric <esr358@gmail.com>
2019-09-06 01:28:02 -0700

    index

--- Design/animation.js
+++ Design/animation.js
@@ -1,16 +1,26 @@
+   
+
 $(document).ready (function(){
+     
+    
+    var tl = new TimelineLite();
+    tl.staggerTo("item.transition", .5, {opacity:'1', marginTop: '0px', delay:.5}, 0.05);
+   
+    
     TweenMax.from(".landing-img", .5, {opacity: '0', ease:Power2.easeInOut, repeat:0, delay:1});
     
     TweenMax.from('.landing-img > img', .5, {marginTop: '-100px', ease:Power2.easeInOut, repeat:0, delay:1});
     
     TweenMax.from('.heading-text', .5, {marginTop: '100px', ease:Power2.easeInOut, repeat:0, delay:1});
     
-    TweenMax.from('.title', 1.5, {opacity: '0', marginLeft: '-100px' ,ease:Power2.easeInOut, repeat:0, delay:1});   
+    TweenMax.from('.title', 1.5, {opacity: '0', marginLeft: '-100px' ,ease:Power2.easeInOut, repeat:0, delay:1}); 
+    
+    TweenMax.from('.subtitle', 1.5, {opacity: '0', marginLeft: '100px' ,ease:Power2.easeInOut, repeat:0, delay:1}); 
+    
+    TweenMax.from('.arrow', 1.5, {opacity: '0', marginTop: '-100px' ,ease:Power2.easeIn, repeat:0, delay:4}); 
+    
+
 });
-
-
-
-
 
 
 
