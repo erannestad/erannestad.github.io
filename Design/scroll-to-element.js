44ee331fb79c641afb07a1d2f2658010144ce5c1
Eric <esr358@gmail.com>
2019-09-09 06:48:31 -0700

    info hash fix

--- Design/scroll-to-element.js
+++ Design/scroll-to-element.js
@@ -4,13 +4,17 @@ var scrollToElem = function(element){
     $(window).scrollTo(document.querySelector(element), 1500, {easing:'easeInOutExpo'});
 };
 
+var scrollArrow = function() {
+    $(window).scrollTo(document.querySelector('100px'), 1500, {easing:'easeInOutExpo'});
+};
+
 
 
 // on index load: if url contains #info, then scroll to .info-content
 //var checkHash = function() {
 
-    if (window.location.hash = 'info') {
-        console.log('test');
+    if (window.location.hash === '#info') {
+        console.log(window.location.hash);
 //        var info = window.location.hash.substring(1); //Puts hash in variable, and removes the # character
         
         $(document).ready(function () {
