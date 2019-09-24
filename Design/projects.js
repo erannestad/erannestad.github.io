8a3d5260e3e33d711d16595ad02f4fcc0c64779d
Eric <esr358@gmail.com>
2019-09-23 22:04:20 -0700

    flex

--- Design/projects.js
+++ Design/projects.js
@@ -5,7 +5,7 @@ var ProjectList = [
         client: 'Clowes Fund, Vermont Studio Center',
         categories: ['art'],
         related: ['WTRST', 'everythingIsGoingToHaveToBePutBack', 'exhibitionCatalog'],
-        date: 'April, 2019',
+        date: '2019',
         thumbnail:'design-portfolio/VSC/Full1.2_thumbnail.jpg',
         //////////////////
         description: 'Firmament Arch is part of a growing series of work about the constructed qualities of climate change and our relationship to the natural world in the face of ecological collapse. <p class="subtext">The series reimagines cosmological structures of the past: the pillars of heaven, the firmament, the great deep, the ocean of heaven, etc. as architectural backbones of our future infrastructure and defining forces in our relationship to nature. <br><br> In religious cosmology, the sky was conceptualized as a physical structure – a firmament – that separated earth from the oceans of heavens. Firmament Arch references the architecture of our increasingly necessary biodomes and greenhouses and reimagines them as the firmament structures of tomorrow. The curved backbone of the work houses a network of tanks, tubes, pumps, and nozzles that form an aeroponic growing environment, allowing plants to grow in irregular and confined spaces with minimal water waste or reliance on natural light.</p>',
@@ -22,7 +22,7 @@ var ProjectList = [
         client: 'Whitman College Art Department',
         categories: ['print'],
         related: ['forYourIdeas', 'printArena', ''],
-        date: 'April, 2019',
+        date: '2019',
         thumbnail:'design-portfolio/exhibition-catalog/Catalog_thumbnail.jpg',
         //////////////////
         description: 'Publication documenting the 2018 Whitman Thesis Exhibition: Best Thing Since Sliced Bread. The catalog is the first to document the artists and work of Whitman College\'s annual Senior Thesis Art Exhibition. Zoe Guckenheimer and I carried out the project with the generous financial support of the Whitman College Art Department. <p class="subtext">FULL CATALOG: <a href="design-portfolio/exhibition-catalog/bestthingsinceslicedbread_exhibitioncatalog_reducedsize.pdf" target="_blank">VIEW PDF</a> <br><br>CREDITS: <br>Design, photography, layout by Eric Rannestad and Zoe Guckenheimer. Photography contributions from Eric Rannestad, Kerr Cirrilo, and Anna Dawson. Printed May 2018 by Bridgetown Printing Company in Portland, OR.</p>',
@@ -40,7 +40,7 @@ var ProjectList = [
         client: '',
         categories: ['print', ' branding', ' motion', ' economics'],
         related: ['infographic', 'precisionRestoration', 'everythingIsGoingToHaveToBePutBack'],
-        date: 'December, 2017',
+        date: '2017',
         thumbnail:'design-portfolio/geothermal/geo11_thumbnail.jpg',
         //////////////////
         description: "<b><i>Geothermal</i></b> is a multimedia design and research project that examines the untapped potential of  geothermal energy production in the Western United States. Final materials include a projection mapped installation, kinetic infographic, and report. <p class='subtext'>The final report exists as a technical apendix to the atmospheric installation and infographic. You can find the report <a href='../../design-portfolio/geothermal/Geothermal-Along-the-Pacific-Coast.pdf' target='_blank'>here</a><br><br>The complete installation was on display in December 2016.</p><br><br>",
@@ -58,7 +58,7 @@ var ProjectList = [
         client: 'Walla Walla County Conservation District',
         categories: ['print',' brand'],
         related: ['conservationVideos', 'wwccdWebsite', 'wwccdInfographics'],
-        date: 'January, 2019',
+        date: '2019',
         thumbnail:'design-portfolio/WWCCD/WWCCDlogo/final/wwccdLogo_thumbnail.jpg',
         //////////////////
         description: 'A brand identity redesign project for the Walla Walla County Conservation District. Logo and web redesigned.',
@@ -76,7 +76,7 @@ var ProjectList = [
         client: 'Walla Walla County Conservation District, Washington Association of Conservation Districts',
         categories: ['print',' infographics'],
         related: ['wwccdLogo', 'wwccdWebsite', 'conservationVideos', 'infographic', 'precisionRestoration'],
-        date: 'November, 2018',
+        date: '2018',
         thumbnail:'design-portfolio/WWCCD/Conservation-Plans-Infographic_thumbnail.jpg',
         //////////////////
         description: 'A series of design and outreach material, including: booklet about the new Voluntary Stewardship Program in Washington State, project summarys, and infographics for the 2019 annual report.',
@@ -94,7 +94,7 @@ var ProjectList = [
         client: 'The Nature Conservancy',
         categories: ['economics', ' infographic'],
         related: ['geothermal', 'infographic', 'wwccdInfographics'],
-        date: 'August 2018',
+        date: '2018',
         thumbnail:'design-portfolio/The-Nature-Conservancy/Market-Analysis-Report_thumbnail.jpg',
         //////////////////
         description: "Collection of marketing and outreach material for The Nature Conservancy's Precision Restoration Project. The suite includes an infographic that tells the larger story of Precision Restoration, a still and animated GIF that depicts how one of the seed treatments work, and an animated fire map that was built in ArcGIS. I worked with staff at the economic consulting firm ECONorthwest to researched write a market analysis and feasibility report for the Oregon Nature Conservancy. Market problems, trends, and supply chain of native seed are addressed. The document continues to serve as a technical appendix for the The Nature Conservancy as they bring Precision Restoration Technologies to a larger production scale.",
@@ -112,7 +112,7 @@ var ProjectList = [
         client: '',
         categories: ['print'],
         related: ['printArena', 'exhibitionCatalog', ''],
-        date: 'July, 2016',
+        date: '2016',
         thumbnail:'design-portfolio/journals/all-spines_thumbnail.jpg',
         //////////////////
         description: "For Your Ideas was a project of 33 hand-bound journals. Each journal was sold for $6 at the Colville Street Patisserie over the summer months of 2016. The journals were digitally printed and hand-bound.",
@@ -130,7 +130,7 @@ var ProjectList = [
         client: 'Clement Valla',
         categories: ['print', ' web'],
         related: ['everythingIsGoingToHaveToBePutBack', 'forYourIdeas', 'exhibitionCatalog'],
-        date: 'July, 2018',
+        date: '2018',
         thumbnail:'design-portfolio/print-arena/arenaChannel4-2_thumbnail.jpg',
         //////////////////
         description: "Tool to make printable books from <a href='https://are.na' target='_blank'>Are.na</a> Channels. A modification of Evan Brooks' <a href='https://evanbrooks.info/bindery/' target='_blank'>Bindery.js</a> tool to print websites. Collaboration with Clement Valla. <p class='subtext'><br><a href='https://www.are.na/clement-valla/print-are-na-with-bindery-js'>Are.na Channel</a> | <a href='https://github.com/clementvalla/Are.na_Bindery.js'>Github Repository</a></p><p class='subtext'><br>The Print Are.na tool wraps stripped-down html with new wrappers. The included css file then styles the manipulated html for an easy, customizable, and print-friendly layout.</p>",
@@ -148,7 +148,7 @@ var ProjectList = [
         client: 'Walla Walla County Conservation District',
         categories: ['video'],
         related: ['wwccdLogo', 'wwccdWebsite', 'infographic'],
-        date: 'December, 2018',
+        date: '2018',
         thumbnail:'design-portfolio/WWCCD/StreamRestorationThumbnail.gif',
         //////////////////
         description: '<div class="resp-container"><iframe class="resp-iframe" src="https://player.vimeo.com/video/309361701" allow="autoplay; fullscreen" allowfullscreen=""></iframe></div><div class="resp-container"><br><iframe class="resp-iframe" src="https://player.vimeo.com/video/309347685" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div>',
@@ -166,7 +166,7 @@ var ProjectList = [
         client: 'Walla Walla County Conservation District',
         categories: ['web', ' branding'],
         related: ['wwccdInfographics', 'wwccdLogo', 'birthday'],
-        date: 'January, 2018',
+        date: '2018',
         thumbnail:'design-portfolio/WWCCD/web_thumbnail.jpg',
         //////////////////
         description: '',
@@ -184,7 +184,7 @@ var ProjectList = [
         client: 'Shell House Arts',
         categories: ['web', ' mapping', ' art', ' environment'],
         related: ['firmamentArch', 'WTRST', 'exhibitionCatalog'],
-        date: 'August, 2019',
+        date: '2019',
         thumbnail:'design-portfolio/maps/pan1_background-thumbnail.gif',
         //////////////////
         description: '<p><b><i>Shipwreck</i></b> is an interactive art project.</p><p class="subtext">Visit the website: <a href="http://erannestad.com/my_EverythingMustBePutBack/map5/index.php" target="_blank" >http://erannestad.com/my_EverythingMustBePutBack/map5/index.php</a><br><br>The online artwork uses the format of a tilemap to address and reflect on what is irreversably lost as our natural systems fail. In this instance the map is disassembled and its traditional function altered. Words from past visitors are adrift across the fragmented system, and new visitors can opt to contribute their own.What is irreversably lost as vast and interconnected natural systems fail? How do you unchop a tree?<br><br><p class="subtext" style="margin-left:20px; margin-right:20px;">"Start with the leaves, the small twigs, and the nests that have been shaken, ripped, or broken off by the fall; these must be gathered and attached once again to their respective places. It is not arduous work, unless major limbs have been smashed or mutilated. If the fall was carefully and correctly planned, the chances of anything of the kind happening will have been reduced. Again, much depends upon the size, age, shape, and species of the tree. Still, you will be lucky if you can get through this stages without having to use machinery. Even in the best of circumstances it is a labor that will make you wish often that you had won the favor of the universe of ants, the empire of mice, or at least a local tribe of squirrels, and could enlist their labors and their talents. But no, they leave you to it. They have learned, with time. This is men\'s work."<br><a href="https://designobserver.com/feature/unchopping-a-tree/7857" target="_blank" >- <i>Unchopping A Tree</i>, by M.S. Merwin</a></p> <br><br><div><img style="width:calc(50% - 4px); margin-right: 8px;" src="design-portfolio/maps/pan1_background.gif"><img style="width: calc(50% - 4px)" src="design-portfolio/maps/pan2_background.gif"></div>',
@@ -202,7 +202,7 @@ var ProjectList = [
         client: '',
         categories: ['web', 'illustration'],
         related: ['wwccdWebsite', 'forYourIdeas', ''],
-        date: 'August, 2019',
+        date: '2019',
         thumbnail:'design-portfolio/happy-birthday-website/full-screenshot_thumbnail.jpg',
         //////////////////
         description:'',
@@ -220,7 +220,7 @@ var ProjectList = [
         client: 'Sheehan Gallery',
         categories: ['art',' installation'],
         related: ['firmamentArch', 'geothermal', 'shipwreck'],
-        date: 'May, 2018',
+        date: '2018',
         thumbnail:'design-portfolio/builtscape/WTRST_thumbnail.jpg',
         //////////////////
         description:'Compartmentalizing the way environments are constructed, measured, and grow, <i>Where the Rain Seeps Through</i> reflects a shifting balance between the natural world and built environment. Sections of material adapt a measured and contained aesthetic, while others resist this uniformity and reflect growth, variety, and decay. This tension characterizes the erosion and eventual failure of systems used to organize and quantify our world.',
@@ -238,7 +238,7 @@ var ProjectList = [
         client: '',
         categories: ['infographic','  video', ' data-visualization'],
         related: ['geothermal', 'conservationVideos', 'wwccdInfographics'],
-        date: 'January, 2017',
+        date: '2017',
         thumbnail:'design-portfolio/geothermal/infographic1.gif',
         //////////////////
         description:'<div class="resp-container"><iframe class="resp-iframe" src="https://player.vimeo.com/video/202665623" allow="autoplay; fullscreen" allowfullscreen=""></iframe></div>',
@@ -258,7 +258,7 @@ var ProjectList = [
         client: '',
         categories: ['infographic',' print'],
         related: ['exhibitionCatalog', 'conservationVideos', 'wwccdInfographics'],
-        date: 'January, 2017',
+        date: '2017',
         thumbnail:'design-portfolio/WWCCD/VSP-WorkPlan_WithBackground_thumbnail.png',
         //////////////////
         description:'',
--- Design/projects_add_highlights.js
+++ Design/projects_add_highlights.js
@@ -9,7 +9,7 @@
             
             var addHighlights = function(i) {
                 var iObj = highlights[i];
-                var content = document.querySelector("#content");
+                var content = document.querySelector("#content-highlights");
                 var allCatagories = iObj.categories.toString();
                 var br = '';
                 var subtextBreak = function (){
@@ -21,7 +21,8 @@
                 };
                 subtextBreak();
                 ///write inner html
-                content.innerHTML += "<item class='overview' onclick= 'loadThisHighlight(\"" + iObj.id + "\");'><p class='overview'>" + iObj.title + " </p><div class='filter'><img class='overview' src='" + iObj.thumbnail + "'></div><p class='subtext overview'>" + iObj.client + br + allCatagories + " | " + iObj.date + "</p></item>";
+   
+                 content.innerHTML += "<item class='overview transition' onclick= 'loadThis(\"" + iObj.id + "\");'><img class='overview' src='" + iObj.thumbnail + "'><p class='subtext overview'>" + iObj.title + "<br>" + allCatagories + " | " + iObj.date + "</p></item>";
                 $("img[src='undefined']").remove();
             };
             
