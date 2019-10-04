2a81a3b625ae497d2c84f5cc268566b99b1c7dc9
Eric <esr358@gmail.com>
2019-10-03 17:19:07 -0700

    efelle app

---
+++ Design/applications/efelle/header_efelle.js
@@ -0,0 +1,10 @@
+var Application = {
+        heading1:'efelle creative',
+        heading2:'Application: Part-Time Graphic Designer',
+        body:'Welcome to the portfolio component of my application. The review committee can navigate to the main site <a href="../../index.html">here</a> should they like view additional projects.<br><p class="subtext">ADDITIONAL LINKS:<br>github: <a href="https://github.com/erannestad">github.com/erannestad</a>  |  fine art website: <a href="https://ericrannestad.com">ericrannestad.com</a></p><p>Thank you for your consideration.'
+    };
+
+
+var ApplicationHeading = document.querySelector("#application-heading");
+
+ApplicationHeading.innerHTML += "<h2>" + Application.heading1 + "<p class='subtext'>" + Application.heading2 + "</p></h2><p>" + Application.body + "</p>";
