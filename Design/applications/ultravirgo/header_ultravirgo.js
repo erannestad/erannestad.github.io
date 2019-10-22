c2a076e9fe9ad4299fc10664f4943fb19635ec5c
Eric <esr358@gmail.com>
2019-10-22 04:42:15 -0400

    ultravirgo

---
+++ Design/applications/ultravirgo/header_ultravirgo.js
@@ -0,0 +1,10 @@
+var Application = {
+        heading1:'Ultravirgo',
+        heading2:'Application: Junior Designer',
+        body:'The review committee can navigate to the main site <a href="../../index.html">here</a> should they like view additional projects.'
+    };
+
+
+var ApplicationHeading = document.querySelector("#application-heading");
+
+ApplicationHeading.innerHTML += "<h2>" + Application.heading1 + "<p class='subtext'>" + Application.heading2 + "</p></h2><p>" + Application.body + "</p>";
