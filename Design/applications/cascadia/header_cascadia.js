e95e071c6db44e51bbf2a3fc78356ee4c05cd5d0
Eric <esr358@gmail.com>
2020-01-25 15:26:35 -0800

    update cascadia and geothermal

---
+++ Design/applications/cascadia/header_cascadia.js
@@ -0,0 +1,10 @@
+var Application = {
+        heading1:'Cascadia Consulting',
+        heading2:'Application: Policy and Planning Analyst',
+        body:'This is a curated list of relevant projects. The Cascadia Consulting review committee can navigate to the <a href="../../index.html">main site</a> to view <a href="../../projects.html">all projects</a> if they would like. <br><br> Thank you for your consideration.<br><br>'
+    };
+
+
+var ApplicationHeading = document.querySelector("#application-heading");
+
+ApplicationHeading.innerHTML += "<h2 class='application-title'>" + Application.heading1 + "<p class='mono'>" + Application.heading2 + "</p></h2><p>" + Application.body + "</p>";
--- Design/projects.js
+++ Design/projects.js
@@ -122,7 +122,7 @@ var ProjectList = [
         //////////////////
         description: 'A series of design and outreach material that promotes the new Voluntary Stewardship Program in Washington State. My role was to write,  design, and produce media that describes how the program works and why it is so crucial for landowners to enroll.',
         //////////////////
-        image: ['../../design-portfolio/WWCCD/VSP-Booklet12.jpg','design-portfolio/WWCCD/VSP-Booklet5.jpg','design-portfolio/WWCCD/VSP-Booklet1.jpg'],
+        image: ['design-portfolio/WWCCD/VSP-Booklet12.jpg','design-portfolio/WWCCD/VSP-Booklet5.jpg','design-portfolio/WWCCD/VSP-Booklet1.jpg'],
         //////////////////
         imageSubtext: ['', '','','','','','','','', '','','','','','','','','','','','','','','','','']
     },
