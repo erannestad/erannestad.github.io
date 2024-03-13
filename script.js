c7fefd84b530c3609533e9dd41690d07eb56c9b5
Eric Rannestad <ericrannestad@gmail.com>
2024-03-13 15:24:55 -0600

    update menu and about

--- script.js
+++ script.js
@@ -15,19 +15,21 @@ var menu = Object.assign(
                <li><a href="about.html">About</a></li>
                <br>
                
+               <li class="subtext menu-category">SCULPTURE</li>
                <li><a href="truss.html">Truss</a></li>
                <li><a href="Arch.html">Firmament Arch</a></li>
-               <li><a href="Where-the-Rain-Seeps-Through.html">Where The Rain Seeps Through</a></li>
+               <li><a href="Where-the-Rain-Seeps-Through.html">Builtscape</a></li>
                <li><a href="forms.html">Forms</a></li>
                <br>
                
+               <li class="subtext menu-category">PAINTING</li>
                <li><a href="plotsgridsmodels.html">Plots, Grids, Models</a></li>
                <li><a href="Market-Box.html">Market Box</a></li>
                <!-- <li><a href="Firmament.html">Firmament</a></li> -->
                <!-- <li><a href="Great-Deep.html">The Great Deep</a></li> -->
                <br>
 
-
+               <li class="subtext menu-category">DIGITAL MEDIA</li>
                <li><a href="ruleset-painting.html">Ruleset Painting Tool</a></li>
                <!-- <li><a href="Mapping-The-Firmament.html">Mapping the Firmament</a></li> -->
                <li><a href="http://erannestad.com/shipwreck/map/index.php" target="_blank">Shipwreck</a></li>
